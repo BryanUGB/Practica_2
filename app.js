152f9d55aead13784ede53ef2a3579de3e26a0bc
alexmartinez503 <82968763+alexmartinez503@users.noreply.github.com>
2022-01-28 10:18:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+//boton ocultar o mostrar los div que contienen la lista 
+const hide = document.querySelector('#hideElements');
+const listDiv = document.querySelector('#list');
+
+//costanstes para mostrar elementos de la lita 
+const addItemInput = document.querySelector('#addItem'); //imput 
+const addItemButton= document.querySelector('button#addItemButton'); //button
+
+//eliminar ultimo item 
+const removeItemButton = document.querySelector('button#removeItemButton');
+
+//eliminar elemento seleccionado
+const removeItem = document.querySelector('button#removeItem')
+
+//mostrar y ocultar lista 
+hide.addEventListener('click', () =>{
+    if(listDiv.style.display == 'none'){
+        listDiv.style.display = 'block';
+        hide.textContent= '<<';
+    } else{
+        listDiv.style.display = 'none'
+        hide.textContent ='>>';
+    }
+});
 
